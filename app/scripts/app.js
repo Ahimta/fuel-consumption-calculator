3516cfa8d731be210861a4313650d4a78ef24c84
Abdullah Alansari <ahimta@gmail.com>
2016-01-07 08:31:14 +0300

    move main index.html content to app/views/old.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,5 +22,10 @@ angular.module('fuelCalculator', ['ngRoute'])
       controller: 'CostCtrl',
       controllerAs: 'vm'
     })
+    .when('/', {
+      templateUrl: 'views/old.html',
+      controller: 'MainCtrl',
+      controllerAs: 'vm'
+    })
     .otherwise({redirectTo: '/'})
 }])
