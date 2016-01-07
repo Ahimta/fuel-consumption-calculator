69a3fc4e8a6a920af841686554228fc95830b1cf
Abdullah Alansari <ahimta@gmail.com>
2016-01-07 07:42:51 +0300

    add calculateDistance

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ angular.module('fuelCalculator', []).controller('MainCtrl', ['$window', function
   this.volume = 1
   this.mode = 'distance'
 
+  function calculateDistance (distanceForLiters, litersForDistance, volume)
+  {
+    return distanceForLiters / litersForDistance * volume
+  }
+
   function getLiterPrice (priceType, fuelType)
   {
     if (priceType === 'new') { return fuelType === '91' ? 0.75 : 0.90 }
@@ -12,7 +17,7 @@ angular.module('fuelCalculator', []).controller('MainCtrl', ['$window', function
   }
 
   function calculateByVolume (priceType, fuelType, volume) { return volume * getLiterPrice(priceType, fuelType) }
-  
+
   function getFuelType ()
   {
     return $window.localStorage.getItem('fuelType') || '91'
@@ -25,6 +30,8 @@ angular.module('fuelCalculator', []).controller('MainCtrl', ['$window', function
 
     return calculateByVolume(priceType, fuelType, volume)
   }
+
+  this.calculateDistance = calculateDistance
 
   this.isFuelType = function (fuelType)
   {
