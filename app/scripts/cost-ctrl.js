e72accd8394e173f1dae87f37764ef1486c262ef
Abdullah Alansari <ahimta@gmail.com>
2016-01-08 08:53:52 +0300

    add cost page

---
+++ app/scripts/cost-ctrl.js
@@ -0,0 +1,41 @@
+'use strict'
+
+angular.module('fuelCalculator').controller('CostCtrl', ['fuelService', function (fuelService)
+{
+  var vm = this
+
+  function calculateLitersByPrice (fuelType, price)
+  {
+    var pricePerLiter = fuelService.getLiterPrice('new', fuelType)
+    var volume = price / pricePerLiter
+
+    return volume
+  }
+
+  function calculateDistanceByPrice (fuelType, distanceForLiters, litersForDistance, price)
+  {
+    var volume = calculateLitersByPrice(fuelType, price)
+    var distance = volume * distanceForLiters / litersForDistance
+
+    return distance
+  }
+
+  this.calculateDistance = function (distanceForLiters, litersForDistance, price)
+  {
+    return calculateDistanceByPrice(fuelService.getFuelType(), distanceForLiters, litersForDistance, price)
+  }
+
+  this.calculatePrice = function (priceType, distanceForLiters, litersForDistance, price)
+  {
+    var distance = calculateDistanceByPrice(fuelService.getFuelType(), distanceForLiters, litersForDistance, price)
+    return vm.calculateByDistance(priceType, distanceForLiters, litersForDistance, distance)
+  }
+
+  this.calculatePriceDifference = function (distanceForLiters, litersForDistance, price)
+  {
+    var oldPrice = vm.calculatePrice('old', distanceForLiters, litersForDistance, price)
+    var newPrice = vm.calculatePrice('new', distanceForLiters, litersForDistance, price)
+
+    return newPrice - oldPrice
+  }
+}])
