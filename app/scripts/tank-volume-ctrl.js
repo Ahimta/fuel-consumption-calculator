3f05b1fa621554f3b13d0c924d9320a7830aab09
Abdullah Alansari <ahimta@gmail.com>
2016-01-10 08:03:33 +0300

    refactor(tank-volume)

--- app/scripts/tank-volume-ctrl.js
+++ app/scripts/tank-volume-ctrl.js
@@ -2,10 +2,29 @@
 
 angular.module('fuelCalculator').controller('TankVolumeCtrl', ['fuelService', function (fuelService)
 {
+  var vm = this
+
   this.distanceForLiters = fuelService.getConsumption()
   this.setConsumption = fuelService.setConsumption
-  this.calculateDistance = fuelService.calculateDistance
 
-  this.calculateByDistance = function (priceType, distanceForLiters, litersForDistance, distance)
-  { return fuelService.calculateByDistance(priceType, fuelService.getFuelType(), distanceForLiters, litersForDistance, distance) }
+  this.calculateDistance = function ()
+  {
+    return fuelService.calculateDistance(vm.distanceForLiters, vm.litersForDistance, vm.tankVolume)
+  }
+
+  this.calculatePrice = function (priceType)
+  {
+    var distance = vm.calculateDistance()
+    var fuelType = fuelService.getFuelType()
+
+    return fuelService.calculateByDistance(priceType, fuelType, vm.distanceForLiters, vm.litersForDistance, distance)
+  }
+
+  this.calculatePriceDifference = function ()
+  {
+    var oldPrice = vm.calculatePrice('old')
+    var newPrice = vm.calculatePrice('new')
+
+    return newPrice - oldPrice
+  }
 }])
