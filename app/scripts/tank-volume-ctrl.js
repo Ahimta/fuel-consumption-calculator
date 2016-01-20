0844a875d594bc510c103cbc5927cb2ab1b20c36
Abdullah Alansari <ahimta@gmail.com>
2016-01-20 14:41:49 +0300

    feat(tank-volume): better support for periods

--- app/scripts/tank-volume-ctrl.js
+++ app/scripts/tank-volume-ctrl.js
@@ -1,19 +1,26 @@
 'use strict'
 
 angular.module('fuelCalculator')
-.controller('TankVolumeCtrl', ['fuelService', 'settingsService', function (fuelService, settingsService)
+.controller('TankVolumeCtrl', ['fuelService', 'periodService', 'settingsService', function (fuelService, periodService, settingsService)
 {
   var vm = this
 
   this.litersForDistance = 1
   this.tankVolume = 64
+  this.fillTimes = 1
+
+  this.periodTable = fuelService.getPeriodTable()
+  this.periodMenu = periodService.getPeriodMenu('week')
 
   this.distanceForLiters = settingsService.consumption()
   this.setConsumption = settingsService.consumption
 
   this.calculateDistance = function ()
   {
-    return fuelService.calculateDistanceByVolume(vm.distanceForLiters, vm.litersForDistance, vm.tankVolume)
+    var fillTimesPerDay = vm.periodMenu.getValuePerDay(vm.fillTimes)
+    var volumePerDay = vm.tankVolume * fillTimesPerDay
+
+    return fuelService.calculateDistanceByVolume(vm.distanceForLiters, vm.litersForDistance, volumePerDay)
   }
 
   this.calculatePrice = function (priceType)
