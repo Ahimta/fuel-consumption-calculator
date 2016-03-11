39ff816d636f8182ca45175e3af1f50efee01937
Abdullah Alansari <ahimta@gmail.com>
2016-03-11 13:27:56 +0300

    feat(cache): suggest reload on update

--- app/scripts/alerts-ctrl.js
+++ app/scripts/alerts-ctrl.js
@@ -1,8 +1,27 @@
 'use strict'
 
-angular.module('fuelCalculator').controller('AlertsCtrl', ['settingsService', function (settingsService)
+angular.module('fuelCalculator').controller('AlertsCtrl', ['$window', 'settingsService', function ($window, settingsService)
 {
-  this.alertsRead = function () { return settingsService.alertsRead() }
+  var isNewerVersionAvailable = false
+
+  this.alertsRead = function () { return !isNewerVersionAvailable && settingsService.alertsRead() }
 
   this.dismiss = function () { settingsService.alertsRead(true) }
+
+  this.isNewerVersionAvailable = function () { return isNewerVersionAvailable; }
+
+  this.update = function (event)
+  {
+    event.preventDefault()
+    $window.location.reload()
+  }
+
+  if ($window.applicationCache)
+  {
+    function onUpdateReady() { isNewerVersionAvailable = true }
+
+    $window.applicationCache.addEventListener('updateready', onUpdateReady)
+
+    if ($window.applicationCache.status === $window.applicationCache.UPDATEREADY) { onUpdateReady() }
+  }
 }])
