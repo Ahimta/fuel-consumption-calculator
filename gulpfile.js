5e0ae67e890e6b6b649cdf173c608e72ac893332
Abdullah Alansari <ahimta@gmail.com>
2016-05-13 18:06:56 +0300

    feat(appcache): add Bootstrap fonts

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,8 @@ gulp.task('manifest', () =>
         'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js',
         'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-route.min.js',
         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
-        'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
+        'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/fonts/glyphicons-halflings-regular.woff2'
       ],
       hash: true
      }))
